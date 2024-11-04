0eadb3edd251cd83836d281916fb31efd9434acc
UmairMarwat <umairmarwat123@gmail.com>
2024-11-04 17:23:56 +0500

    myfirsdtreactapa

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import Hoddie from "./assets/images/hoddie.png"
+
+function App() {
+  return (
+   <>
+   <div>
+    <h1>Happy to see our first React app</h1>
+    <img src={Hoddie} alt="" />
+   </div>
+   </>
+  );
+}
+
+export default App;
