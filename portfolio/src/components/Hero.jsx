18296cb24de5e44e856b7961852585cd3fed1e54
UmairMarwat <umairmarwat123@gmail.com>
2024-11-06 17:05:36 +0500

    3rd

---
+++ portfolio/src/components/Hero.jsx
@@ -0,0 +1,8 @@
+
+export default function Hero(){
+    return(
+        <div className="text-white">
+            Hero section
+        </div>
+    )
+}
---
+++ portfolio/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Hero from "../components/Hero"
+
+const Home = () =>{
+return (
+    <div className="bg-black max-h-screen">
+        <Hero />
+    </div>
+)
+}
+
+export default Home;
