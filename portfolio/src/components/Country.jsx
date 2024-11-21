2d40473c5c87d82b417739af60c5e112cf92cd4a
UmairMarwat <umairmarwat123@gmail.com>
2024-11-21 17:19:30 +0500

    chnges

--- portfolio/src/components/Country.jsx
+++ portfolio/src/components/Country.jsx
@@ -3,17 +3,31 @@ import axios from "axios";
 
 export default function Country() {
     const [data, setData] = useState(null);
+    const [filterData ,setFilterData] = useState([])
+    const [search , setSearch] = useState("")
 
     useEffect(() => {
         axios
             .get("https://restcountries.com/v3.1/all")
             .then((response) => {
                 setData(response.data);
+                setFilterData(response.data)
             })
             .catch((error) => {
                 console.log("API not calling", error);
             });
     }, []);
+
+    const handleSearch = (e) => {
+        const searchValue = e.target.value.toLowerCase();
+        setSearch(searchValue);
+    
+        const filtered = data.filter((country) =>
+            country.name.common.toLowerCase().includes(searchValue)
+        );
+        setFilterData(filtered);
+    };
+    
 
     // Check if data is loaded before trying to render
     if (!data) {
@@ -24,23 +38,28 @@ export default function Country() {
         <div>
             <div className="bg-black my-5">
                 <h3 className="text-5xl text-center">All countries</h3>
+                <input type="text"
+                value={search}
+                onChange={handleSearch}
+                placeholder="Search the country here"
+                className="text-3xl rounded-lg" />
                 <div className="flex flex-col gap-5">
-                    {data.map((i) => (
-                        <div  className="flex flex-col gap-5">
+                    {filterData.map((country) => (
+                        <div   className="flex flex-col gap-5">
                             <h3 className="text-3xl font-bold text-green-300">
-                                Country: {i.name.common}
+                                Country: {country.name.common}
                             </h3>
                             <h3 className="text-3xl font-bold text-green-300">
-                                Country: {i.name.official}
+                                Country: {country.name.official}
                             </h3>
                             <h3 className="text-3xl font-bold text-green-300">
-                                Population: {i.population}
+                                Population: {country.population}
                             </h3>
                             <h3 className="text-3xl font-bold text-green-300">
-                                Capital: {i.capital }
+                                Capital: {country.capital }
                             </h3>
                             <img
-                                src={i.flags.png}
+                                src={country.flags.png}
                                 
                                 className="w-32"
                             />
